17591575c8f02742e81d283ac0b8c2a3f6d84684
Greg Morgan <34179799+vibraniumforge@users.noreply.github.com>
2019-05-13 13:16:30 -0700

    Update app.rb

--- app.rb
+++ app.rb
@@ -1,15 +1,10 @@
 class App < Sinatra::Base
-<<<<<<< HEAD
 
   # Define your GET '/' route below and respond with "Hello, World!"
+  
   get '/' do
     "Hello, World!"
   end
 
 end
-=======
-  get '/' do
-    "Hello, World!"
-  end
-end
->>>>>>> solution with contributing file and .learn tags
+
